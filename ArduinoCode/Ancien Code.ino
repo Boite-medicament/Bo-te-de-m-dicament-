42f034d14bca17c63b0b01a1c14b602eb5d4f3ef
Groupe 8 <104845037+Boite-medicament@users.noreply.github.com>
2022-05-29 16:22:07 +0200

    Update Ancien Code.ino

--- ArduinoCode/Ancien Code.ino
+++ ArduinoCode/Ancien Code.ino
@@ -25,6 +25,34 @@ SDA to UCA pin A4 (or the SDA pin for your arduino)
 */
 
 
+
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
+
+// LoRaWAN end-device address (DevAddr)
+
+static const u4_t DEVADDR = 0x260BA87A;
+
+// LoRaWAN NwkSKey, network session key
+// This is the default Semtech key, which is used by the early prototype TTN
+// network.
+static const PROGMEM u1_t NWKSKEY[16] = { 0x5E, 0x27, 0x90, 0x83, 0x42, 0xF1, 0x40, 0xF7, 0xED, 0x82, 0xFA, 0x19, 0x26, 0x13, 0x34, 0xE8 };
+
+
+// LoRaWAN AppSKey, application session key
+// This is the default Semtech key, which is used by the early prototype TTN
+// network.
+static const u1_t PROGMEM APPSKEY[16] = { 0x29, 0x85, 0xFF, 0xA6, 0x08, 0x62, 0xCA, 0x88, 0x68, 0xED, 0x79, 0xAE, 0x29, 0x24, 0x08, 0x97 };
+
+
+// These callbacks are only used in over-the-air activation, so they are
+// left empty here (we cannot leave them out completely unless
+// DISABLE_JOIN is set in config.h, otherwise the linker will complain).
+void os_getArtEui (u1_t* buf) { }
+void os_getDevEui (u1_t* buf) { }
+void os_getDevKey (u1_t* buf) { }
+
 // Include libraries:
 #include "lcdgfx.h"
 #include <Servo.h>
@@ -41,10 +69,7 @@ int potpin = 0;                           // analog pin used to connect the pote
 int val;                                  // variable to read the value from the analog pin
 long duration;                            // extended variable, time taken by the signal of the ultrassonic sensor to be emitted then received
 int distance;                             // integer (= entier), distance between the sensor and the object in front of it
-int duree1;                               // duration from the launch of the program until the last rotation of the servomotor
-int duree2;                               // duration since the launch of the program until now 
-int delai;                                // duration between the last rotation of the servomotor until now 
-int intervalle;                           // time during which the servomotor is blocked
+
 
 void setup() {
   myservo.attach(A0);                     // attach myservo to pin A0
@@ -54,8 +79,6 @@ void setup() {
   display.begin();
   display.fill(0x00);
   display.setFixedFont(ssd1306xled_font6x8);
-  duree1 = millis();
-  intervalle = 20000;
   }
 
 
@@ -67,52 +90,33 @@ void loop() {
   digitalWrite(trigPin, LOW);             // set the trigPin back at LOW 
   duration = pulseIn(echoPin, HIGH);      // time taken by the signal of the ultrassonic sensor to be emitted then received, in microseconds 
   delay(100);
-  distance = duration * 0.034 / 2;        // distance between the sensor and the object in front of it, in centimeters
-  Serial.print(distance);                 // print the distance in the monitor
+  distance = (duration * 0.034 / 2) + 3;        // distance between the sensor and the object in front of it, in centimeters
+  Serial.print(distance);                       // print the distance in the monitor
+  static uint8_t mydata[] = "";
+  static uint8_t mydata[] = mydata + distance;
   val = map(val, 0, 1023, 0, 180);
+  myservo.write(180);
   delay(200);
-  
-  if(distance < 10)                                                                       // if someone put his in front of the sensor 
+ 
+  if(distance < 10)                                                                      // if someone put his in front of the sensor 
   {
     Serial.println(" ");
-    duree2 = millis();
-    delai = duree2 - duree1;
-    if  (delai >= intervalle)                                                             // the interval has been respected, the user must take his medicine
-    {
-      duree1 = millis();
-      display.printFixedN (0, 16, "                         ", STYLE_BOLD, FONT_SIZE_2X); // erase the old message
-      display.printFixedN (0, 32, "             ", STYLE_BOLD, FONT_SIZE_2X);
-      display.printFixedN (0, 16, "Medicament Pris ", STYLE_BOLD, FONT_SIZE_2X);          // display the sentence on the screen
-      myservo.write(90);                                                                  // the servomoteur rotate in a 90 degres angle then take his originale place back
-      delay(200);
-      myservo.write(0);
-      delay(3000);
-    }                                                            
-    else                                                                                  // the interval isn't respected, the servomotor don't turn 
-    {
-      display.printFixedN (0, 16, "                         ", STYLE_BOLD, FONT_SIZE_2X); // erase the old message
-      display.printFixedN (0, 16, " DEJA PRIS ", STYLE_BOLD, FONT_SIZE_2X);               // display the sentence on the screen
-      delay(1000);
-    }
-  }
+    display.printFixedN (0, 16, "                         ", STYLE_BOLD, FONT_SIZE_2X); // erase the old message
+    display.printFixedN (0, 32, "             ", STYLE_BOLD, FONT_SIZE_2X);
+    display.printFixedN (0, 16, "Medicament   Pris ", STYLE_BOLD, FONT_SIZE_2X);          // display the sentence on the screen
+    myservo.write(90);                                                                  // the servomoteur rotate in a 90 degres angle then take his originale place back
+    delay(200);
+    myservo.write(180);
+    delay(10000);
+   }
   
   else                                                                                    // the screen will show if the medicine must be taken or has already been 
   {
-    Serial.println(" ");
-    duree2 = millis();
-    delai = duree2 - duree1;
-    if (delai >= intervalle )
-    {
-      display.printFixedN (0, 16, "                    ", STYLE_BOLD, FONT_SIZE_2X);
-      display.printFixedN (0, 32, "                   ", STYLE_BOLD, FONT_SIZE_2X);
-      display.printFixed (0, 16, "   Prenez votre  ", STYLE_BOLD);  
-      display.printFixedN (0, 32, "medicament", STYLE_BOLD, FONT_SIZE_2X);
-      delay(100);
-    }
-    else
-    {
-      display.printFixedN (0, 16, "Medicament Pris     ", STYLE_BOLD, FONT_SIZE_2X);          
-      delay(100);
-    }
+   Serial.println(" ");
+   display.printFixedN (0, 16, "                    ", STYLE_BOLD, FONT_SIZE_2X);
+   display.printFixedN (0, 32, "                   ", STYLE_BOLD, FONT_SIZE_2X);
+   display.printFixedN (0, 16, " Prendre", STYLE_BOLD,  FONT_SIZE_2X);  
+   display.printFixedN (0, 32, "Medicament", STYLE_BOLD, FONT_SIZE_2X);
+   delay(100);
   }
 }
